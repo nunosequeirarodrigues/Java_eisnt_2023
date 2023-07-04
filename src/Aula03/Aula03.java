6d76b180e373e6e1d9b53028f11aa1c5bf1bc8bc
Nuno Rodrigues <nunosequeira.rodrigues@gmail.com>
2023-07-04 20:32:29 +0100

    Aula03 Herança

---
+++ src/Aula03/Aula03.java
@@ -0,0 +1,27 @@
+package Aula03;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        System.out.println("Aula 03 >");
+
+        Aula03 ref = new Aula03();
+
+        ref.java_loops( );
+
+        System.out.printf("Aula 03 <");
+    }
+
+    public void java_loops( ) {
+
+       String[] cars = {"Volvo", "BMW", "Ford"};
+
+       for  (String item : cars) {
+            System.out.println(i);
+       }
+
+
+    }
+
+
+}
+
