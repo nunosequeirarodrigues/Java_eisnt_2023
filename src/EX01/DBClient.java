dffe66e6074f5b81e4325bbd46d2be1246021e46
Nuno Rodrigues <nunosequeira.rodrigues@gmail.com>
2023-07-06 23:22:49 +0100

    Aula04 23:08

--- src/EX01/DBClient.java
+++ src/EX01/DBClient.java
@@ -1,24 +1,19 @@
 package EX01;
 
 public interface DBClient {
-<<<<<<< HEAD
+
     public void connect() throws DBException;
 
     public Boolean isConnected();
 
     public void disconnect() throws DBException;
-=======
-    public void connect();
 
->>>>>>> origin/master
 
     public void read();
 
     public void write();
 
-<<<<<<< HEAD
+
     public void list() throws DBException;
-=======
-    public void list();
->>>>>>> origin/master
+
 }
